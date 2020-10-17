61cd526c8064122ae29b1a2bff3c3e287e388ee1
shryke2a <shryke2a@hotmail.fr>
2020-10-17 21:37:19 +0200

    1st commit
    Non functionnal :
    -Identifying X Y Z of face
    -Sending data to opentrack

---
+++ app/src/main/java/com/example/androidopenfacetracking/MainActivity.kt
@@ -0,0 +1,236 @@
+package com.example.androidopenfacetracking
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.util.Size
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.mlkit.common.MlKitException
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetectorOptions
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.IOException
+import java.net.*
+import java.nio.ByteBuffer
+import java.nio.channels.IllegalBlockingModeException
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+typealias LumaListener = (luma: Double) -> Unit
+
+
+    class MainActivity : AppCompatActivity() {
+        private var imageCapture: ImageCapture? = null
+
+        private lateinit var outputDirectory: File
+        private lateinit var cameraExecutor: ExecutorService
+        private var trackingState = false
+
+        //Instance d'analyseur d'image
+        //TODO instanciate img size in data
+        private var processedImgSize = Size(480, 360)
+
+        private lateinit var imageAnalysis: ImageAnalysis
+
+        // instance de processing du facetrack
+        // Preparation des options de detections de visage
+        private val faceDetectOpts = FaceDetectorOptions.Builder()
+            .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
+            .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_ALL)
+            .setContourMode(FaceDetectorOptions.CONTOUR_MODE_ALL)
+            .build()
+
+        private val faceDetector = FaceDetection.getClient(faceDetectOpts)
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+            imageAnalysis = ImageAnalysis.Builder()
+                .setTargetResolution(processedImgSize)
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    @SuppressLint("UnsafeExperimentalUsageError")
+    fun trackingToggle(v: View) {
+        val b: Button = v as Button
+
+        trackingState = !trackingState
+
+        if(trackingState) {
+            b.text = getString(R.string.button_tracking_running)
+
+            //Init UDP socket
+        try {
+                val port: Int = Integer.parseInt(txtPort.text.toString())
+                val ip: InetAddress = InetAddress.getByName(txtAddr.text.toString())
+                val addr: InetSocketAddress = InetSocketAddress(ip, port)
+                val udpSocket: DatagramSocket = DatagramSocket()
+
+            imageAnalysis.setAnalyzer(
+                ContextCompat.getMainExecutor(this),
+                ImageAnalysis.Analyzer { imageProxy ->
+                    val rotationDegrees = imageProxy.imageInfo.rotationDegrees
+                    val image: InputImage = InputImage.fromMediaImage(
+                        imageProxy.image!!,
+                        rotationDegrees
+                    )
+                    try {
+                        faceDetector.process(image)
+                            .addOnSuccessListener { faces ->
+                                var iter = 0
+                                for (face in faces) {
+                                    iter += 1
+
+                                    if (iter == 1) {
+                                        pitch_axis_display.text = face.headEulerAngleX.toString()
+                                        yaw_axis_display.text = face.headEulerAngleY.toString()
+                                        roll_axis_display.text = face.headEulerAngleZ.toString()
+
+                                        var buf: ByteBuffer = ByteBuffer.allocate(Double.SIZE_BYTES * 6)
+                                        buf.putDouble(0.0) //X
+                                        buf.putDouble(0.0) //Y
+                                        buf.putDouble(0.0) //Z
+                                        buf.putDouble(face.headEulerAngleX.toDouble()) //Yaw
+                                        buf.putDouble(face.headEulerAngleY.toDouble()) //Pitch
+                                        buf.putDouble(face.headEulerAngleZ.toDouble()) //Roll
+
+                                        val array = buf.array()
+
+                                        val d: DatagramPacket = DatagramPacket(
+                                            array,
+array.size,
+                                            addr
+                                        )
+                                        //TODO fix send method
+                                        // udpSocket.send(d)
+                                        Log.e(TAG, "Message to be sent: $d")
+                                    }
+                                }
+                                number_of_face.text = iter.toString()
+                                // Log.e(TAG, faces.toString())
+
+                            }
+                            .addOnFailureListener { e ->
+                                Log.e(TAG, "Face detection failed $e")
+                            }
+                            .addOnCompleteListener {
+                                imageProxy.close()
+                            }
+                    } catch (e: MlKitException) {
+                        Log.e(
+                            TAG,
+                            "Failed to process image. Error: " + e.localizedMessage
+                        )
+                    }
+                })
+            }
+            catch(e: SocketException){
+                Log.e(TAG, "Socket went wrong $e")
+            }
+        }
+
+        else {
+            imageAnalysis.clearAnalyzer()
+            b.text = getString(R.string.button_tracking_stopped)
+        }
+
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener(Runnable {
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(viewFinder.surfaceProvider)
+                }
+
+            // Select back camera as a default
+            //TODO Set a camera selector
+            val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, imageAnalysis, preview
+                )
+
+            } catch (exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    companion object {
+        private const val TAG = "CameraXBasic"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray
+    ) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+}
