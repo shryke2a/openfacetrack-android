1fc178c57923200969c95bc8c20c5c2a586ff117
shryke2a <shryke2a@hotmail.fr>
2020-10-31 17:48:06 +0100

    BEST PERFORMANCE ACHIEVED
    Network executing on a separate thread
    Camera analysis executing on another separate thread
    
    Non functionnal :
    -Identifying X Y Z of face

--- app/src/main/java/com/example/androidopenfacetracking/MainActivity.kt
+++ app/src/main/java/com/example/androidopenfacetracking/MainActivity.kt
@@ -22,9 +22,6 @@ import com.google.mlkit.vision.face.FaceDetection
 import com.google.mlkit.vision.face.FaceDetectorOptions
 import kotlinx.android.synthetic.main.activity_main.*
 import java.net.*
-import java.nio.ByteBuffer
-import java.nio.ByteOrder
-import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -35,11 +32,12 @@ typealias LumaListener = (luma: Double) -> Unit
     class MainActivity : AppCompatActivity() {
 
         private lateinit var cameraExecutor: ExecutorService
-        private lateinit var networkExecutor: ExecutorService
+        private var networkExecutor: ExecutorService = Executors.newSingleThreadExecutor()
+        private var camExecutor: ExecutorService = Executors.newSingleThreadExecutor()
         private var trackingState = false
 
         //Instance d'analyseur d'image
-        //TODO instanciate img size in data
+        //TODO instantiate img size in data
         private var processedImgSize = Size(480, 360)
 
         private lateinit var imageAnalysis: ImageAnalysis
@@ -49,8 +47,10 @@ typealias LumaListener = (luma: Double) -> Unit
         //TODO Test best speed
         private val faceDetectOpts = FaceDetectorOptions.Builder()
             .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
-            .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_ALL)
-            .setContourMode(FaceDetectorOptions.CONTOUR_MODE_ALL)
+            // .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_ALL)
+            .setContourMode(FaceDetectorOptions.CONTOUR_MODE_NONE)
+            .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_NONE)
+            .setMinFaceSize(0.3f)
             .build()
 
         private val faceDetector = FaceDetection.getClient(faceDetectOpts)
@@ -82,95 +82,60 @@ typealias LumaListener = (luma: Double) -> Unit
         trackingState = !trackingState
 
         if(trackingState) {
-            b.text = getString(R.string.button_tracking_running)
+                b.text = getString(R.string.button_tracking_running)
 
-            //Init UDP socket
-                val port: Int = Integer.parseInt(txtPort.text.toString())
-                val ip: InetAddress = InetAddress.getByName(txtAddr.text.toString())
-                val addr: InetSocketAddress = InetSocketAddress(ip, port)
-                val udpSocket: DatagramSocket = DatagramSocket()
+                //Init UDP socket
+                    val port: Int = Integer.parseInt(txtPort.text.toString())
+                    val ip: InetAddress = InetAddress.getByName(txtAddr.text.toString())
+                    val addr: InetSocketAddress = InetSocketAddress(ip, port)
+                    val udpSocket: DatagramSocket = DatagramSocket()
+                    val net: netWork = netWork(udpSocket, addr)
+                    // Frame counter
+                    var frameCount: Int = 0
 
-                // Frame counter
-                var frameCount: Long = 0
-
-            imageAnalysis.setAnalyzer(
-                ContextCompat.getMainExecutor(this),
-                ImageAnalysis.Analyzer { imageProxy ->
-                    val rotationDegrees = imageProxy.imageInfo.rotationDegrees
-                    val image: InputImage = InputImage.fromMediaImage(
-                        imageProxy.image!!,
-                        rotationDegrees
-                    )
-                    try {
-                        faceDetector.process(image)
-                            .addOnSuccessListener { faces ->
-                                var iter = 0
-                                for (face in faces) {
-                                    iter += 1
-
-                                    if (iter == 1) {
-                                        frameCount += 1
-                                        pitch_axis_display.text = face.headEulerAngleX.toString()
-                                        yaw_axis_display.text = face.headEulerAngleY.toString()
-                                        roll_axis_display.text = face.headEulerAngleZ.toString()
-
-                                        //TODO find out why the package aren't understood
-                                        // After test, packages are sent to the target computer with the right number of bytes.
-                                        // But opentrack doesn't seem to understand the data.
-
-                                        var buf: ByteBuffer = ByteBuffer.allocate(Double.SIZE_BYTES * 6)
-
-                                        buf.order(ByteOrder.LITTLE_ENDIAN)
-
-                                        buf.putDouble(0.0) //X
-                                        buf.putDouble(0.0) //Y
-                                        buf.putDouble(0.0) //Z
-                                        buf.putDouble(face.headEulerAngleY.toDouble()) //Yaw
-                                        buf.putDouble(face.headEulerAngleX.toDouble()) //Pitch
-                                        buf.putDouble(face.headEulerAngleZ.toDouble()) //Roll
-
-                                        /* buf.putDouble(12.27) //X
-                                        buf.putDouble(9.7) //Y
-                                        buf.putDouble(15.5) //Z
-                                        buf.putDouble(33.4) //Yaw
-                                        buf.putDouble(27.6) //Pitch
-                                        buf.putDouble(22.4) //Roll */
-
-                                        //Log.e(TAG, "Buffer after alloc ${Arrays.toString(buf.array())}.")
-
-                                        val d: DatagramPacket = DatagramPacket(buf.array(), buf.array().size, addr)
-
-                                        /* Log.e(TAG, "Angle X detected : ${face.headEulerAngleZ}")
-                                        Log.e(TAG, "Angle X sent : ${ByteBuffer.wrap(d.data).getDouble(40)}") */
-
-                                        //TODO Check efficiency: maybe faster way
-                                        networkExecutor = Executors.newSingleThreadExecutor()
-
-                                        networkExecutor.execute {
-                                            udpSocket.send(d)
-                                            // Log.e(TAG, "data sent : ${Arrays.toString(buf.array())}")
-                                        }
-
-                                        networkExecutor.shutdown()
-                                    }
-                                }
-                                number_of_face.text = frameCount.toString()
-                                // Log.e(TAG, faces.toString())
-
-                            }
-                            .addOnFailureListener { e ->
-                                Log.e(TAG, "Face detection failed $e")
-                            }
-                            .addOnCompleteListener {
-                                imageProxy.close()
-                            }
-                    } catch (e: MlKitException) {
-                        Log.e(
-                            TAG,
-                            "Failed to process image. Error: " + e.localizedMessage
+                imageAnalysis.setAnalyzer(
+                    camExecutor,
+                    ImageAnalysis.Analyzer { imageProxy ->
+                        val rotationDegrees = imageProxy.imageInfo.rotationDegrees
+                        val image: InputImage = InputImage.fromMediaImage(
+                            imageProxy.image!!,
+                            rotationDegrees
                         )
-                    }
-                })
+                        try {
+                            faceDetector.process(image)
+                                .addOnSuccessListener { faces ->
+                                    var iter = 0
+                                    for (face in faces) {
+                                        iter += 1
+
+                                        if (iter == 1) {
+
+                                            frameCount += 1
+
+                                            pitch_axis_display.text = face.headEulerAngleX.toString()
+                                            yaw_axis_display.text = face.headEulerAngleY.toString()
+                                            roll_axis_display.text = face.headEulerAngleZ.toString()
+
+                                            networkExecutor.execute {
+                                                net.calcAndSend(face.headEulerAngleY.toDouble(), face.headEulerAngleX.toDouble(), face.headEulerAngleZ.toDouble(), face.boundingBox)
+                                                Log.e(TAG, face.boundingBox.toString())
+                                            }
+                                        }
+                                    }
+                                    number_of_face.text = frameCount.toString()
+                                    // Log.e(TAG, faces.toString())
+
+                                }
+                                .addOnCompleteListener {
+                                    imageProxy.close()
+                                }
+                        } catch (e: MlKitException) {
+                            Log.e(
+                                TAG,
+                                "Failed to process image. Error: " + e.localizedMessage
+                            )
+                        }
+                    })
 
 
         }
